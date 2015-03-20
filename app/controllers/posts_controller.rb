f677714c45b1801ca18b4a3dd43d425f5a22b537
Arnold Lei <arnold.lei88@gmail.com>
2015-03-20 15:41:18 -0400

    fixed the 'no routes' error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,14 @@
+#Routes 
+# post_comments POST   /posts/:post_id/comments(.:format)     comments#create
+#  post_comment GET    /posts/:post_id/comments/:id(.:format) comments#show
+#         posts GET    /posts(.:format)                       posts#index
+#               POST   /posts(.:format)                       posts#create
+#      new_post GET    /posts/new(.:format)                   posts#new
+#     edit_post GET    /posts/:id/edit(.:format)              posts#edit
+#          post GET    /posts/:id(.:format)                   posts#show
+#               PATCH  /posts/:id(.:format)                   posts#update
+#               PUT    /posts/:id(.:format)                   posts#update
+
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
